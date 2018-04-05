4ea341cdfb411a8ace04e38d09dc41d7e800d56d
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-04-05 11:27:04 +0200

    level 1 done

--- level1/teachers_json_writter.rb
+++ level1/teachers_json_writter.rb
@@ -14,12 +14,72 @@ end
 
 auto_incremented_id = auto_increment_teacher_id(data["teachers"])
 
+p "- - - Welcome on Mymentor.com ! - - -"
+p"*"
+p"**"
+p"***"
+p "what's your first name ?"
+print ">"
+firstname = gets.chomp
+p"***"
+p"**"
+p"*"
+p"*"
+p"**"
+p"***"
+p "what's your last name ?"
+print ">"
+lastname = gets.chomp
+p"***"
+p"**"
+p"*"
+p"*"
+p"**"
+p"***"
+p "what kind of fields do you teach ? (please write the number associated to your field)"
+p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+p "1 - mathematics"
+p "2 - english"
+p "3 - history"
+p "4 - biology"
+p "5 - physics"
+p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+print ">"
+fields = gets.chomp.to_i
+p"***"
+p"**"
+p"*"
+p"*"
+p"**"
+p"***"
+p "what is your level of teaching for this field ?"
+p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+p "1 - grade 6th or cycle middle"
+p "2 - 7th, --> cycle middle"
+p "3 - 8th, --> cycle middle"
+p "4 - 9th, --> cycle high"
+p "5 - 10th, --> cycle high"
+p "6 - 11th, --> cycle high"
+p "7 - 12th, --> cycle high"
+p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+print ">"
+levels = gets.chomp.to_i
+p"***"
+p"**"
+p"*"
+p"*"
+p"**"
+p"***"
+p "Thanks your '#{firstname}' #{lastname}, your profil registered successfully !"
+p"***"
+p"**"
+p"*"
 new_teacher = {
     "id" => auto_incremented_id,
-    "firstname" =>  'Albert',
-    "lastname" => 'Einstein',
-    "fields" =>  1,
-    "levels" => 7
+    "firstname" => firstname ,
+    "lastname" => lastname,
+    "fields" =>  fields,
+    "levels" => levels
   }
 
   data["teachers"] << new_teacher
