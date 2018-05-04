28c8df6af5d6c8417f00835c0c883e1bf984f2b7
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-05-04 10:06:32 +0200

    refacto

--- level1/teachers_json_writter.rb
+++ level1/teachers_json_writter.rb
@@ -1,80 +1,77 @@
-require 'json'
+  require 'json'
 
-filepath = '../data.json'
-serialized_data = File.read(filepath)
-data = JSON.parse(serialized_data)
+  filepath = '../data.json'
+  serialized_data = File.read(filepath)
+  data = JSON.parse(serialized_data)
 
-def auto_increment_teacher_id(teacher_array)
-  id_array = []
-  teacher_array.each do |teacher|
-    id_array << teacher["id"]
+  def auto_increment_teacher_id(teacher_array)
+    id_array = []
+    teacher_array.each { |teacher| id_array << teacher["id"] }
+    id_array.empty? ? auto_increment_teacher_id = 1 : auto_increment_teacher_id = id_array.max + 1
   end
-  id_array.empty? ? auto_increment_teacher_id = 1 : auto_increment_teacher_id = id_array.max + 1
-end
 
-auto_incremented_id = auto_increment_teacher_id(data["teachers"])
+  auto_incremented_id = auto_increment_teacher_id(data["teachers"])
 
-skills_hash = {}
-skills_array = []
+  skills_hash = {}
+  skills_array = []
 
-
-#view
-p "- - - Welcome on Mymentor.com ! - - -"
-p "Hello dear new professor"
-p "what's your first name ?"
-print ">"
-firstname = gets.chomp.capitalize
-
-p "what's your last name ?"
-print ">"
-lastname = gets.chomp.capitalize
-
-choice = "y"
-until choice == "n"
-  p "what kind of fields do you teach ?(please write the number associated to your field)"
-  p "."
-  p "1 - mathematics"
-  p "2 - english"
-  p "3 - history"
-  p "4 - biology"
-  p "5 - physics"
-  p "."
+  #view
+  p "- - - Welcome on Mymentor.com ! - - -"
+  p "Hello dear new professor"
+  p "what's your first name ?"
   print ">"
-  fields = gets.chomp.to_i
+  firstname = gets.chomp.capitalize
 
-  p "what is your level of teaching for this field ?(please write the number associated to your field)"
-  p "."
-  p "1 - grade 6th or cycle middle"
-  p "2 - 7th, --> cycle middle"
-  p "3 - 8th, --> cycle middle"
-  p "4 - 9th, --> cycle high"
-  p "5 - 10th, --> cycle high"
-  p "6 - 11th, --> cycle high"
-  p "7 - 12th, --> cycle high"
-  p "."
+  p "what's your last name ?"
   print ">"
-  levels = gets.chomp.to_i
-    skills_array << skills_hash = {
-      "fields": fields,
-      "levels": levels
+  lastname = gets.chomp.capitalize
+
+  choice = "y"
+  until choice == "n"
+    p "what kind of fields do you teach ?(please write the number associated to your field)"
+    p "."
+    p "1 - mathematics"
+    p "2 - english"
+    p "3 - history"
+    p "4 - biology"
+    p "5 - physics"
+    p "."
+    print ">"
+    fields = gets.chomp.to_i
+
+    p "what is your level of teaching for this field ?(please write the number associated to your field)"
+    p "."
+    p "1 - grade 6th or cycle middle"
+    p "2 - 7th, --> cycle middle"
+    p "3 - 8th, --> cycle middle"
+    p "4 - 9th, --> cycle high"
+    p "5 - 10th, --> cycle high"
+    p "6 - 11th, --> cycle high"
+    p "7 - 12th, --> cycle high"
+    p "."
+    print ">"
+    levels = gets.chomp.to_i
+      skills_array << skills_hash = {
+        "fields": fields,
+        "levels": levels
+        }
+
+    p  'Do you want to add another field ? (y/n)'
+    choice = gets.chomp
+  end
+
+  p "Thanks your #{firstname} #{lastname}, your profil registered successfully !"
+  p "Goodbye !"
+
+  new_teacher = {
+      "id" => auto_incremented_id,
+      "firstname" => firstname ,
+      "lastname" => lastname,
+      "skills" => skills_array
     }
 
-  p  'Do you want to add another field ? (y/n)'
-  choice = gets.chomp
-end
+    data["teachers"] << new_teacher
 
-p "Thanks your #{firstname.capitalize} #{lastname.capitalize }, your profil registered successfully !"
-p "Goodbye !"
-
-new_teacher = {
-    "id" => auto_incremented_id,
-    "firstname" => firstname ,
-    "lastname" => lastname,
-    "skills" => skills_array
-  }
-
-  data["teachers"] << new_teacher
-
-File.open(filepath, 'w') do |file|
-  file.write(JSON.generate(data))
-end
+  File.open(filepath, 'w') do |file|
+    file.write(JSON.generate(data))
+  end
--- level2/request_json_writter.rb
+++ level2/request_json_writter.rb
@@ -1,92 +1,82 @@
-require 'json'
+  require 'json'
 
-filepath = '../data.json'
-serialized_data = File.read(filepath)
-data = JSON.parse(serialized_data)
+  filepath = '../data.json'
+  serialized_data = File.read(filepath)
+  data = JSON.parse(serialized_data)
 
 
-p "- - - Welcome on Mymentor.com ! - - -"
-p "Hello dear new student"
-p "What's your full name ?"
+  p "- - - Welcome on Mymentor.com ! - - -"
+  p "Hello dear new student"
+  p "What's your full name ?"
 
-print ">"
-fullname = gets.chomp.capitalize
+  print ">"
+  fullname = gets.chomp.capitalize
 
-p "what kinf of fields do you want to learn ?(please write the number associated to your field)"
-p "."
-p "1 - mathematics"
-p "2 - english"
-p "3 - history"
-p "4 - biology"
-p "5 - physics"
-p "."
-print ">"
-fields = gets.chomp.to_i
+  p "what kinf of fields do you want to learn ?(please write the number associated to your field)"
+  p "."
+  p "1 - mathematics"
+  p "2 - english"
+  p "3 - history"
+  p "4 - biology"
+  p "5 - physics"
+  p "."
+  print ">"
+  fields = gets.chomp.to_i
 
-p "what is your level in this field ?"
-p "."
-p "1 - grade 6th or cycle middle"
-p "2 - 7th, --> cycle middle"
-p "3 - 8th, --> cycle middle"
-p "4 - 9th, --> cycle high"
-p "5 - 10th, --> cycle high"
-p "6 - 11th, --> cycle high"
-p "7 - 12th, --> cycle high"
-p "."
-print ">"
-levels = gets.chomp.to_i
+  p "what is your level in this field ?"
+  p "."
+  p "1 - grade 6th or cycle middle"
+  p "2 - 7th, --> cycle middle"
+  p "3 - 8th, --> cycle middle"
+  p "4 - 9th, --> cycle high"
+  p "5 - 10th, --> cycle high"
+  p "6 - 11th, --> cycle high"
+  p "7 - 12th, --> cycle high"
+  p "."
+  print ">"
+  levels = gets.chomp.to_i
 
-p "thanks for this informations"
-sleep(1)
-p "we are searching the best professor for you"
-sleep(2)
-p "just 3 more seconds"
-sleep(1)
-p "3"
-sleep(1)
-p "2"
-sleep(1)
-p "1"
-sleep(1)
-p "Tada ! 🎉  we have found the best teacher for you"
+  p "thanks for this informations"
+  sleep(1)
+  p "we are searching the best professor for you"
+  sleep(2)
+  p "just 3 more seconds"
+  sleep(1)
+  p "3"
+  sleep(1)
+  p "2"
+  sleep(1)
+  p "1"
+  sleep(1)
+  p "Tada ! 🎉  we have found the best teacher for you"
 
-all_teachers = data["teachers"]
-student_teacher_list = []
-choice_array = []
+  all_teachers = data["teachers"]
+  student_teacher_list = []
+  choice_array = []
 
-all_teachers.each do |teacher_hash|
-  teacher_hash["skills"].each do |skill|
-    skill["fields"]
-    skill["levels"]
-    if skill["fields"] == fields && skill["levels"] == levels
-     student_teacher_list << teacher_hash
-    else
+  all_teachers.each do |teacher_hash|
+    teacher_hash["skills"].each do |skill|
+      student_teacher_list << teacher_hash if skill["fields"] == fields && skill["levels"] == levels
     end
   end
-end
-p "This is the list of professor you match with :"
-student_teacher_list.each do |list|
-  p "#{list['id']} - #{list['firstname']} #{list['lastname']}"
-end
-p "wich one is your favorite ? (please write the number associated to your field) "
-print ">"
-choice_teacher = gets.chomp.to_i - 1
-student_teacher_list.each_with_index do |twin, index|
-  if index == choice_teacher
-    choice_array << twin
-  else
+  p "This is the list of professor you match with :"
+  student_teacher_list.each { |list| p "#{list['id']} - #{list['firstname']} #{list['lastname']}" }
+  p "wich one is your favorite ? (please write the number associated to your field) "
+  print ">"
+  choice_teacher = gets.chomp.to_i - 1
+  student_teacher_list.each_with_index do |twin, index|
+     choice_array << twin if index == choice_teacher
   end
-end
-p "thank you for your choice"
+  p "thank you for your choice"
 
-new_request = {
-    "fullname" => fullname ,
-    "fields" => fields,
-    "levels" => levels,
-    "reference" => choice_array
-  }
+  new_request = {
+      "fullname" => fullname ,
+      "fields" => fields,
+      "levels" => levels,
+      "reference" => choice_array
+    }
 
-  data["requests"] << new_request
-File.open(filepath, 'w') do |file|
-  file.write(JSON.generate(data))
-end
+    data["requests"] << new_request
+  File.open(filepath, 'w') do |file|
+    file.write(JSON.generate(data))
+  end
